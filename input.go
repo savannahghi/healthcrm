011acac55fda5c785dd40196f3139511c083eb54
BiKodes <bikocodes@gmail.com>
2024-04-17 09:34:24 +0300

    chore(CPL-319): Closing open server requests and code refactor

--- input.go
+++ input.go
@@ -4,26 +4,26 @@ import "fmt"
 
 // Facility is the hospitals data class
 type Facility struct {
-	ID                                 string                                `json:"id,omitempty"`
-	Name                               string                                `json:"name,omitempty"`
-	Description                        string                                `json:"description,omitempty"`
-	FacilityType                       string                                `json:"facility_type,omitempty"`
-	County                             string                                `json:"county,omitempty"`
-	Country                            string                                `json:"country,omitempty"`
-	Address                            string                                `json:"address,omitempty"`
-	Coordinates                        *Coordinates                          `json:"coordinates,omitempty"`
-	Contacts                           []Contacts                            `json:"contacts,omitempty"`
-	Identifiers                        []Identifiers                         `json:"identifiers,omitempty"`
-	BusinessHours                      []BusinessHours                       `json:"businesshours,omitempty"`
+	ID            string          `json:"id,omitempty"`
+	Name          string          `json:"name,omitempty"`
+	Description   string          `json:"description,omitempty"`
+	FacilityType  string          `json:"facility_type,omitempty"`
+	County        string          `json:"county,omitempty"`
+	Country       string          `json:"country,omitempty"`
+	Address       string          `json:"address,omitempty"`
+	Coordinates   *Coordinates    `json:"coordinates,omitempty"`
+	Contacts      []Contacts      `json:"contacts,omitempty"`
+	Identifiers   []Identifiers   `json:"identifiers,omitempty"`
+	BusinessHours []BusinessHours `json:"businesshours,omitempty"`
 }
 
 // Coordinates represents geographical coordinates using latitude and longitude.
 // Latitude measures the north-south position, while longitude measures
 // the east-west position.
 type Coordinates struct {
-	Latitude                            string                                `json:"latitude,omitempty"`
-	Longitude                           string                                `json:"longitude,omitempty"`
-	Radius                              string                                `json:"radius,omitempty"`
+	Latitude  string `json:"latitude,omitempty"`
+	Longitude string `json:"longitude,omitempty"`
+	Radius    string `json:"radius,omitempty"`
 }
 
 // ToString returns the location in comma-separated values format.
@@ -40,67 +40,67 @@ func (c Coordinates) ToString() (string, error) {
 
 // Contacts models facility's model data class
 type Contacts struct {
-	ContactType                          string                                  `json:"contact_type,omitempty"`
-	ContactValue                         string                                  `json:"contact_value,omitempty"`
-	Role                                 string                                  `json:"role,omitempty"`
-} 
+	ContactType  string `json:"contact_type,omitempty"`
+	ContactValue string `json:"contact_value,omitempty"`
+	Role         string `json:"role,omitempty"`
+}
 
 // Identifiers models facility's identifiers; can be MFL Code, Slade Code etc...
 type Identifiers struct {
-	IdentifierType                       string                                   `json:"identifier_type,omitempty"`
-	IdentifierValue                      string                                   `json:"identifier_value,omitempty"`
-	ValidFrom                            string                                   `json:"valid_from,omitempty"`
-	ValidTo                              string                                   `json:"valid_to,omitempty"`
+	IdentifierType  string `json:"identifier_type,omitempty"`
+	IdentifierValue string `json:"identifier_value,omitempty"`
+	ValidFrom       string `json:"valid_from,omitempty"`
+	ValidTo         string `json:"valid_to,omitempty"`
 }
 
 // BusinessHours models data to store business hours
 type BusinessHours struct {
-	Day                                  string                                    `json:"day"`
-	OpeningTime                          string                                    `json:"opening_time"`
-	ClosingTime                          string                                    `json:"closing_time"`
+	Day         string `json:"day"`
+	OpeningTime string `json:"opening_time"`
+	ClosingTime string `json:"closing_time"`
 }
 
 // Pagination is used to hold pagination values
 type Pagination struct {
-	Page                                 string                                     `json:"page"`
-	PageSize                             string                                     `json:"page_size"`
+	Page     string `json:"page"`
+	PageSize string `json:"page_size"`
 }
 
 // FacilityServiceInput models is used to create a new service
 type FacilityServiceInput struct {
-	Name                                 string                                     `json:"name"`
-	Description                          string                                     `json:"description"`
-	Identifiers                          []*ServiceIdentifierInput                  `json:"identifiers"`
+	Name        string                    `json:"name"`
+	Description string                    `json:"description"`
+	Identifiers []*ServiceIdentifierInput `json:"identifiers"`
 }
 
 // ServiceIdentifierInput is used to create an identifier
 type ServiceIdentifierInput struct {
-	IdentifierType                       string                                      `json:"identifier_type"`
-	IdentifierValue                      string                                      `json:"identifier_value"`
+	IdentifierType  string `json:"identifier_type"`
+	IdentifierValue string `json:"identifier_value"`
 }
 
 // ProfileInput is the host of users data or a brief description of a person
 type ProfileInput struct {
-	FirstName                            string                                   `json:"first_name"`
-	LastName                             string                                   `json:"last_name"`
-	OtherName                            string                                   `json:"other_name"`
-	DateOfBirth                          string                                   `json:"date_of_birth"`
-	Gender                               string                                   `json:"gender"`
-	EnrolmentDate                        string                                   `json:"enrolment_date"`
-	SladeCode                            string                                   `json:"slade_code"`
-	ServiceCode                          string                                   `json:"service_code"`
-	Contacts                             []*ProfileContactInput                   `json:"contacts,omitempty"`  
-	Identifiers                          []*ProfileIdentifierInput                `json:"identifiers,omitempty"`
+	FirstName     string                    `json:"first_name"`
+	LastName      string                    `json:"last_name"`
+	OtherName     string                    `json:"other_name"`
+	DateOfBirth   string                    `json:"date_of_birth"`
+	Gender        string                    `json:"gender"`
+	EnrolmentDate string                    `json:"enrolment_date"`
+	SladeCode     string                    `json:"slade_code"`
+	ServiceCode   string                    `json:"service_code"`
+	Contacts      []*ProfileContactInput    `json:"contacts,omitempty"`
+	Identifiers   []*ProfileIdentifierInput `json:"identifiers,omitempty"`
 }
 
-//  ProfileIdentifierInput is used to create profile(s) identifier(s)
+// ProfileIdentifierInput is used to create profile(s) identifier(s)
 type ProfileIdentifierInput struct {
-	IdentifierValue                      string                                   `json:"identifier_value"`
-    IdentifierType                       string                                   `json:"identifier_type"`
+	IdentifierValue string `json:"identifier_value"`
+	IdentifierType  string `json:"identifier_type"`
 }
 
-//  ProfileContanctInput is used to create profile(s) contact(s)
+// ProfileContanctInput is used to create profile(s) contact(s)
 type ProfileContactInput struct {
-	ContactType                           string                                   `json:"contact_type"`
-    ContactValue                          string                                   `json:"contact_value"`
+	ContactType  string `json:"contact_type"`
+	ContactValue string `json:"contact_value"`
 }
